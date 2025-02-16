c7a89f5a114f76c15fbda9a442793e0880d33cca
Pablo Ulloa <pablo.ulloac@usach.cl>
2025-02-16 19:48:03 +0000

    Added support for PHP, Ruby, Zig and Universal

---
+++ src/universal-alpine/test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+zsh --version
+git --version
+gh --version
+vim --version
+nvim --version
+tmux -V
+bun version
+npm --version
+node --version
+go --version
+java --version
+mvn --version
+gradle --version
+groovy --version
+php --version
+python --version
+ruby --version
+cargo version
+zig version
